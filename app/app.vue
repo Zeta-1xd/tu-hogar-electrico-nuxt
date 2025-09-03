7fad6355a2b9990d4b7ab4c1c0dfd144994c5099
Abraham <hola@holasolucionelectricas.cl>
2025-09-02 23:37:53 -0400

    Proyecto Nuxt listo para Cloudflare

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold">Servicios Eléctricos Profesionales</h1>
+    <p>Bienvenido a nuestra web en Nuxt</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
